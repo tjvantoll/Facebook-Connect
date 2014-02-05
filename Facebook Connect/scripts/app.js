f646c55faa78a65d4a50c5d2cf7a616f81a52f4e
TJ VanToll <TJ.VanToll@telerik.com>
2014-02-05 12:03:13 -0600

    Initial commit

--- Facebook Connect/scripts/app.js
+++ Facebook Connect/scripts/app.js
@@ -1,24 +1,36 @@
-(function (global) {
-    var mobileSkin = "",
-        app = global.app = global.app || {};
+/******************************************************************/
+// global error handling
+/******************************************************************/
+function showAlert( message, title, callback ) {
+    window.alert( message, callback || function () {}, title, "OK" );
+};
+function showError( message ) {
+    showAlert( message, "Error occurred" );
+};
+window.addEventListener( "error", function ( e ) {
+    e.preventDefault();
+    var message = e.message + "' from " + e.filename + ":" + e.lineno;
+    showAlert( message, "Error occurred" );
+    return true;
+});
 
-    document.addEventListener('deviceready', function () {
-        navigator.splashscreen.hide();
-        $(document.body).height(window.innerHeight);
-    }, false);
 
-    app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout"});
 
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
+function setupFacebook() {
+    FB.init({
+        appId: "204075246457176",
+        status: true,
+        xfgml: true
+    });
+    
+    FB.getLoginStatus(function( response ) {
+        alert( JSON.stringify( response ) );
+    });
+};
 
-        app.application.skin(mobileSkin);
-    };
-})(window);
+document.addEventListener( "deviceready", function () {
+    navigator.splashscreen.hide();
+    $( document.body ).height( window.innerHeight );
+    new kendo.mobile.Application( document.body, { layout: "layout" });
+    setupFacebook();
+});
